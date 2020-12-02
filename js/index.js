7b6eca0649fde80be8121d90957f2b8dbb57bbed
user.email <stephanie.enciso13@gmail.com>
2020-12-01 18:23:55 -0600

    Created variables for the h2 tags.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+
+const h2Welcome = document.querySelector('.intro h2');
+const h2Go = document.querySelector('.text-content h2');
+const h2Advent = document.querySelector('.text-content h2');
