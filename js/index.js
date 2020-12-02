054d88dac0b7b890223996f8bac570c8042853f6
user.email <stephanie.enciso13@gmail.com>
2020-12-01 18:55:17 -0600

    Finished declaring the variables.

--- js/index.js
+++ js/index.js
@@ -2,4 +2,11 @@
 
 const h2Welcome = document.querySelector('.intro h2');
 const h2Go = document.querySelector('.text-content h2');
-const h2Advent = document.querySelector('.text-content h2');
+const h2Pick = document.querySelector('.content-destination h2');
+const homeDiv = document.querySelector('.home');
+const goDiv = document.querySelector('.text-content');
+const  pickDiv = document.querySelector('.content-destination');
+const goImg = document.querySelector('.img-content');
+const adventImg = document.querySelector('.img-fluid');
+const  h1Logo = document.querySelector('h1');
+const navBar = document.querySelector('nav');
